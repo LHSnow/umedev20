3789fd4b2473d5ab2fd9d2b74ae8c363e2075908
Lars-Henrik Snow <lars@harhund.com>
2020-05-27 12:54:32 +0200

    efter refaktorisering

--- src/app/2048/spelplan.spec.ts
+++ src/app/2048/spelplan.spec.ts
@@ -1,6 +1,6 @@
 import { Spelplan } from './spelplan';
 
-fdescribe('Spelplan', () => {
+describe('Spelplan', () => {
   let spelplan: Spelplan;
 
   beforeEach(() => {
@@ -9,22 +9,18 @@ fdescribe('Spelplan', () => {
 
   describe(`När två brickor med samma valör kolliderar
             bildar de en ny bricka med summan av de två`, () => {
-    it('Ex: 4200< => 4200', () => {
-      spelplan.plan[0] = [4, 2, 0, 0];
-      spelplan.kollidera();
-      expect(spelplan.plan[0]).toEqual([4, 2, 0, 0]);
-    });
+    function ex(input, expectation) {
+      it(`${input}< => ${expectation}`, () => {
+        spelplan.plan[0] = input;
+        spelplan.kollidera();
+        expect(spelplan.plan[0]).toEqual(expectation);
+      });
+    }
 
-    it('Ex: 2200< => 4000', () => {
-      spelplan.plan[0] = [2, 2, 0, 0];
-      spelplan.kollidera();
-      expect(spelplan.plan[0]).toEqual([4, 0, 0, 0]);
-    });
+    ex([4, 2, 0, 0], [4, 2, 0, 0]);
+    ex([2, 2, 0, 0], [4, 0, 0, 0]);
+    ex([4, 4, 2, 2], [8, 4, 0, 0]);
 
-    it('Ex: 4422< => 8400', () => {
-      spelplan.plan[0] = [4, 4, 2, 2];
-      spelplan.kollidera();
-      expect(spelplan.plan[0]).toEqual([8, 4, 0, 0]);
-    });
+
   });
 });
